43beb66991b3886f758460a00bc56345d3d207c8
Uriel Rodriguez Usma <ssurielyugo@gmail.com>
2023-01-06 14:30:12 -0500

    Create catalog repository

--- src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace Catalog.API.Repositories
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories
 {
     public interface IProductRepository
     {
+        Task<IReadOnlyList<Product>> GetProducts();
+        
+        Task<Product> GetProduct(string id);
+        Task<IReadOnlyList<Product>> GetProductByName(string name);
+        Task<IReadOnlyList<Product>> GetProductByCategory(string categoryName);
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
     }
 }
